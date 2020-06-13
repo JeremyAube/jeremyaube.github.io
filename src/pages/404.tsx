052e900b9fb531abb3a4c4b143a81321e3ae0e59
Jeremy Aube <jeremyaube24@gmail.com>
2020-06-12 23:09:58 -0300

    fixed 404 pages and added gp-pages package

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from "src/components/templates/Layout";
 
 export default function NotFoundPage() {
-	<Layout>
-		<h1>OOPS..</h1>
-		<p>Cette page n'a pas été trouvé!</p>
-	</Layout>;
+	return (
+		<>
+			<h1>OOPS..</h1>
+			<p>Cette page n'a pas été trouvé!</p>
+			<Link to="/">Retour</Link>
+		</>
+	);
 }
